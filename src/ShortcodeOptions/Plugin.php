df79fd78ff9fe4417c47b11723e473aee0ad4250
Uriel Wilson <devuri@users.noreply.github.com>
2022-10-28 12:09:03 -0500

    2.0.0 (#1)
    * 2.0.0
    
    * feat: Adds build tools
    
    * fix name
    
    * php8
    
    * ..

---
+++ src/ShortcodeOptions/Plugin.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of the WP Shortcode Options WordPress PLugin.
+ *
+ * (c) Uriel Wilson <uriel@urielwilson.com>
+ *
+ * Please see the LICENSE file that was distributed with this source code
+ * for full copyright and license information.
+ */
+
+namespace ShortcodeOptions;
+
+use ShortcodeOptions\Traits\Singleton;
+
+class Plugin
+{
+    use Singleton;
+
+    /**
+     * Start here.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Add Shortcode [wpoption opt="myoption"].
+     */
+    public function hooks(): void
+    {
+        add_shortcode( 'wpoption', [ $this, 'wpoption_shortcode' ] );
+    }
+
+    /**
+     * Create Shortcode output.
+     *
+     * @param array $atts
+     *
+     * @return 
+     */
+    public function wpoption_shortcode( $atts )
+    {
+        $atts = shortcode_atts(
+            [
+                'opt' => 'blogname',
+            ],
+            $atts
+        );
+
+        ob_start();
+
+        echo esc_html( get_option( $atts['opt'] ) );
+
+        return ob_get_clean();
+    }
+}

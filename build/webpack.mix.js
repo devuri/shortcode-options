9ed848e29b42a7f85fa3942f47dd576e187b16f2
Uriel Wilson <devuri@users.noreply.github.com>
2022-12-22 18:50:40 -0500

    trunk as build dir (#11)

--- build/webpack.mix.js
+++ build/webpack.mix.js
@@ -1,17 +1,17 @@
 let mix = require('laravel-mix')
 
-mix.setPublicPath('build').version()
+mix.setPublicPath('trunk').version()
 
 mix.options({ manifest: false })
 
 // build the plugin files in trunk.
-mix.copy('vendor', 'build/trunk/vendor')
-mix.copy('languages', 'build/trunk/languages')
-mix.copy('src', 'build/trunk/src')
+mix.copy('vendor', 'trunk/vendor')
+mix.copy('languages', 'trunk/languages')
+mix.copy('src', 'trunk/src')
 
 mix.copy([
     'index.php',
     'LICENSE',
     'readme.txt',
     'shortcode-options.php',
-], 'build/trunk/')
+], 'trunk/')

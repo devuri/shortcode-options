5c0cb5ad80e8a1391117db22f61ec13c0824f31f
devuri <devuriko@gmail.com>
2022-12-22 22:24:18 -0500

    chore: revert build directory and Add `.distignore` for deploy

--- build/webpack.mix.js
+++ build/webpack.mix.js
@@ -5,13 +5,13 @@ mix.setPublicPath('trunk').version()
 mix.options({ manifest: false })
 
 // build the plugin files in trunk.
-mix.copy('vendor', 'trunk/vendor')
-mix.copy('languages', 'trunk/languages')
-mix.copy('src', 'trunk/src')
+mix.copy('vendor', 'build/trunk/vendor')
+mix.copy('languages', 'build/trunk/languages')
+mix.copy('src', 'build/trunk/src')
 
 mix.copy([
     'index.php',
     'LICENSE',
     'readme.txt',
     'shortcode-options.php',
-], 'trunk/')
+], 'build/trunk/')

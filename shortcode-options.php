7efe0d366ae0f99807f42e47eca76c7416747930
devuri <devuri@users.noreply.github.com>
2024-07-27 21:49:40 -0500

    fix: tested up to update `wp-6.6`

--- shortcode-options.php
+++ shortcode-options.php
@@ -1,16 +1,9 @@
 <?php
 /**
- * Shortcode Options.
- *
- * @author            Uriel Wilson
- * @copyright         2015 - 2022 Uriel Wilson
- * @license           GPL-2.0
- *
- * @wordpress-plugin
  * Plugin Name:       Shortcode Options
  * Plugin URI:        https://urielwilson.com/
  * Description:       The Shortcode Options plugin is a simple way to display WordPress Options using shortcode <strong>[wpoption opt="blogname"]</strong>, you can reference list of options here:<a href="https://codex.wordpress.org/Option_Reference"> Option Reference </a>.
- * Version:           2.3.3
+ * Version:           2.3.4
  * Requires at least: 3.4
  * Requires PHP:      7.3
  * Author:            uriel
